dcf771ee84694755f9c18e9e6bb756ea0fd5232e
thomastosoni <tosoni.thomas@gmail.com>
2022-04-18 19:02:19 +0200

    DataFrames and SparkSQL

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,18 +11,37 @@ object Main {
   }
 
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("Samples").setMaster("local")
-    val sc = SparkContext.getOrCreate(conf)
-//    val spark = SparkSession.builder
-//      .appName("Simple Application")
-//      .config("spark.master", "local")
-//      .config("spark.some.config.option", "some-value")
-//      .getOrCreate()
+//    val conf = new SparkConf().setAppName("Samples").setMaster("local")
+//    val sc = SparkContext.getOrCreate(conf)
+//
+//    val xRangeRDD = sc.parallelize(1 to 50000, 4)
+//    xRangeRDD.cache()
+//
+//    time { xRangeRDD.count() }
+//    time { xRangeRDD.count() }
 
-    val xRangeRDD = sc.parallelize(1 to 50000, 4)
-    xRangeRDD.cache()
+    val spark = SparkSession.builder
+      .appName("Simple Application")
+      .config("spark.master", "local")
+      .config("spark.some.config.option", "some-value")
+      .getOrCreate()
 
-    time { xRangeRDD.count() }
-    time { xRangeRDD.count() }
+    val df = spark.read.json("src/main/resources/people.json").cache()
+    df.show()
+    df.printSchema()
+
+    df.createTempView("people")
+
+    df.select("name").show()
+    df.select(df("name")).show()
+    spark.sql("SELECT name FROM people").show()
+
+    df.filter(df("age") > 21).show()
+    spark.sql("SELECT age, name FROM people WHERE age > 21").show()
+
+    df.groupBy("age").count().show()
+    spark.sql("SELECT age, COUNT(age) as count FROM people GROUP BY age").show()
+
+    spark.close()
   }
 }
